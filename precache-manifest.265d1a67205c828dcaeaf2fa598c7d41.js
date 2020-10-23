4c9625c79046c805d54a4d7b9648a0751f535efc
EvanMa <yuchen0626ma@gmail.com>
2020-10-23 12:11:50 +1100

    Updates

---
+++ precache-manifest.265d1a67205c828dcaeaf2fa598c7d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a6e68ec205e2597b70f3bc4e5a64fd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "af73c23461421c4e264d",
+    "url": "/monster-rolodex/static/css/main.765d03f9.chunk.css"
+  },
+  {
+    "revision": "4f1c17ebbebcba4fc330",
+    "url": "/monster-rolodex/static/js/2.a917164d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/monster-rolodex/static/js/2.a917164d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af73c23461421c4e264d",
+    "url": "/monster-rolodex/static/js/main.0037cb25.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
